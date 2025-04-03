e6998c0a8cf0e1351614c341788836d52f698050
Raymond Hebert <Rjhebert@email.neit.edu>
2025-04-03 09:09:35 -0400

    CU

---
+++ week2/js/script.js
@@ -0,0 +1,3 @@
+var name = prompt("What is your name???");
+document.getElementById("greeting").innerHTML = 'Hello $(name) how are you today?';
+document.getElementById("greeting").style.color = 'red';
